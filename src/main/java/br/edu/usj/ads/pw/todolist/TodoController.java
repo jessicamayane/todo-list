f6d9251603267af7d088f1443a77d6d3f93eb6bc
jmayanes <jessica.mayane.almeida@everis.com>
2021-11-05 12:08:39 -0300

    Adição de extensão Lombok para integração com banco de dados postgres, agora a lista processa e volta pra raiz, exibindo a informação inserida na Todo List.

--- src/main/java/br/edu/usj/ads/pw/todolist/TodoController.java
+++ src/main/java/br/edu/usj/ads/pw/todolist/TodoController.java
@@ -1,8 +1,8 @@
 package br.edu.usj.ads.pw.todolist;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +13,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TodoController {
 
-    List<String> list = new ArrayList<>();
+    
+    //List<String> list = new ArrayList<>();
+
+    @Autowired
+    TodoRepository todoRepository;
 
     @GetMapping(value="/")
     public ModelAndView getIndex() {
+        //buscar todos do repositorio e criar uma lista com eles
+        List<Todo> list = todoRepository.findAll();
+
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("todo_list", list);
         return modelAndView;
@@ -25,11 +32,18 @@ public class TodoController {
 
 
     @PostMapping(value="/add")
-    public ModelAndView postAdd(@RequestParam String todo) {
-        list.add(todo);
+    public String postAdd(@RequestParam String todo) {
+        //list.add(todo);
     
-        ModelAndView modelAndview = new ModelAndView("index");
-        modelAndview.addObject("todo_list", list);
-        return modelAndview;    }
-    
-}
+        Todo newTodo = new Todo();
+        newTodo.setDescription(todo);
+        // 1- Cria o objeto,  pega o que vem do form ("todo") e seta na descrição 
+
+        // 2- adicionar no repository e do repository joga no banco de dados
+        todoRepository.save(newTodo);
+        //apagou o mv, apenas para retornar a raiz após a adição de tarefa
+        // colocou String no lugar de mv .. postadd
+        //foi chamado, adicionado a atarefa e enviado pra raiz.
+        return "redirect:/";
+    } 
+}    
